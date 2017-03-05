3d3302ba98c198ebb52b248ab8d2a6cfb954379f
ThomasLaigneau <Thomas@MacBook-Pro-de-Thomas.local>
2017-03-05 21:11:02 +0100

    Travail sur ls design de l'interface admin

--- professeur/index.php
+++ professeur/index.php
@@ -74,8 +74,8 @@ while ($donnees = $reponse->fetch())
 <form action="traitement/add/ajoutchapitre.php" method="post" class="ajout-chapitre">
 	<input type="text" name="titre" placeholder="Titre" class="input-titre">
 	<br />
-	  Lien vers theme :
-	  	<select name="lien">
+	  <p class="link-theme-titre">Lien vers theme :</p>
+	  	<select name="lien" class="link-theme-chap">
 	  <?php
 $lientheme = $bdd->query('SELECT * FROM theme'); //affichage des themes present dans la BDD
 
@@ -87,11 +87,12 @@ while ($donnees = $lientheme->fetch())
 
 ?>
 		</select><br />
-			  Id des cours :
-	<input type="text" name="lienchapitre">
+			  <p class="id-cours">Id des cours :</p>
+	<input type="text" name="lienchapitre" placeholder="Id cours" class="idtext">
 	<br />
 	<br />
 	<input class="submit" type="submit" value="Créer le Chapitre">
+	</form>
 </div>
 
 <div id="colonne2">
